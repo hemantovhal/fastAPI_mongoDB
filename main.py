d4c321018247cf00800f8b4d1ee36a294339334e
hemantovhal <hemantovhal31093@gmail.com>
2022-12-19 19:24:19 +0530

    CRUD operations performed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World", "first_name": "Hemant", "last_name":"Ovhal"}
 
 
 @app.get("/items/{item_id}")
